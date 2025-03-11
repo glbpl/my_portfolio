f5b13fbaddb9538ec7985ee36a166f18cf541405
Axios-88 <m72409@yandex.ru>
2025-03-11 21:59:08 +0500

    build(deps-dev): add rollup and plugins for bundling
    Add Rollup and related plugins as dev dependencies to enable proper module bundling:
    - @rollup/plugin-commonjs
    - @rollup/plugin-node-resolve
    - @rollup/plugin-terser
    - rollup-plugin-css-only
    - Configure rollup.config.mjs with these plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,20 @@
-// rollup.config.mjs
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import css from 'rollup-plugin-css-only';
+
 export default {
 	input: 'src/main.js',
 	output: {
 		file: 'dist/bundle.js',
-		format: 'iife'
-	}
+		format: 'iife',
+		name: 'bundle',
+	},
+	plugins: [
+		resolve(),
+		commonjs(),
+		css({ output: 'bundle.css' }),
+		//terser()
+	]
 };
